a838c1629841dd71ae5dc826d185f3787618968b
Andreas Fehn <fehnomenal@fehn.systems>
2023-10-29 11:41:31 +0100

    first release

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import { readFile } from 'node:fs/promises';
+import { basename } from 'node:path';
+
+const PREFIX = '\0bundle-native:';
+
+/** @type {import('.').bundleNative} */
+export function bundleNative() {
+  /** @type {string[]} */
+  const nativeFileRefs = [];
+
+  return {
+    name: 'bundle-native',
+
+    resolveId(id) {
+      if (id.endsWith('.node')) {
+        return PREFIX + id;
+      }
+    },
+
+    async load(id) {
+      if (id.startsWith(PREFIX)) {
+        const ref = this.emitFile({
+          type: 'asset',
+          name: basename(id),
+          source: await readFile(id.slice(PREFIX.length)),
+        });
+
+        nativeFileRefs.push(ref);
+
+        return {
+          code: [
+            `import { createRequire } from 'node:module';`,
+            `const require = createRequire(import.meta.url);`,
+            `export default require(import.meta.ROLLUP_FILE_URL_${ref});`,
+          ].join('\n'),
+        };
+      }
+    },
+
+    resolveFileUrl: {
+      order: 'pre',
+      handler({ referenceId, relativePath }) {
+        if (nativeFileRefs.includes(referenceId)) {
+          return JSON.stringify(`./${relativePath}`);
+        }
+      },
+    },
+  };
+}
