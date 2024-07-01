a5787d3a283b9c0a98906ff071403920afbdd0b5
Andreas Fehn <fehnomenal@fehn.systems>
2024-07-01 12:04:27 +0200

    resolve more modern libsql versions for better treeshaking

--- src/resolve-libsql/index.js
+++ src/resolve-libsql/index.js
@@ -5,15 +5,21 @@ import MagicString from 'magic-string';
 export function resolveLibsql() {
   const target = currentTarget();
 
+  const finalRequire = 'require(`@libsql/${target}`);';
+  const modernRequire = 'requireNative();';
+
+  const resolvedRequire = `require('@libsql/${target}').default;`;
+
   return {
     name: 'resolve-libsql',
 
     async transform(code) {
-      if (code.includes('require(`@libsql/${target}`)')) {
-        const str = new MagicString(code).replace(
-          'require(`@libsql/${target}`)',
-          `require('@libsql/${target}').default`,
-        );
+      if (code.includes(finalRequire)) {
+        const str = new MagicString(code);
+
+        str
+          .replaceAll(finalRequire, resolvedRequire)
+          .replaceAll(modernRequire, resolvedRequire);
 
         return { code: str.toString(), map: str.generateMap() };
       }
